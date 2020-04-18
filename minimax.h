d793817a0c8cc1f246c68e146b37914df3cc8e67
ElRego1 <ElRego1@users.noreply.github.com>
2020-04-18 15:20:41 +0300

    Reordered an include so the ide will see the class

--- minimax.h
+++ minimax.h
@@ -2,9 +2,9 @@
 #define MINIMAX_H
 
 #include <bits/stdc++.h>
+#include "game.h"
 #include "constants.h"
 #include "pieces.h"
-#include "game.h"
 
 int alphabeta_maxi(int depth, int alpha, int beta, Game &g);
 std::vector<std::vector<char>> get_moves_m(Game &g); // calls get_moves from my perspective
