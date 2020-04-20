6f40cd9a817d890be98183bbbba55a9cf74f27fd
ElRego1 <ElRego1@users.noreply.github.com>
2020-04-20 15:34:45 +0300

    Solved a few warnings
    Co-Authored-By: Oprea Olivia <oliviawoow@users.noreply.github.com>

--- game.cpp
+++ game.cpp
@@ -43,7 +43,7 @@ std::vector<char> Game::find_next_move() {
       score = alphabeta_mini(DEPTH - 1, alpha, beta, *this);
     }
 
-    if (score = HIGH) { // if we find this it means we will give checkmate
+    if (score == HIGH) { // if we find this it means we will give checkmate
       std::cout << "#ATENTIE: Am gasit un mod de a castiga cu miscarea: (" << move[0] << ", " << move[1] << ") -> (" << move[2] 
         << ", " << move[3] << "); piece: " << this->m_board[move[0]][move[1]] << " -> " << this->m_board[move[2]][move[3]] << std::endl;
         return move;
@@ -410,13 +410,18 @@ std::vector<char> attacks(char i, char j, std::vector<std::vector<char>> &chess_
 }
 
 bool Game::is_check_m() {
-  char x, y;
+  char x = -1, y = -1;
   for (auto &v : m_pieces) {
     if (v[2] == 7) {  // my king position
       x = v[0];
       y = v[1];
       break;
     }
+  }
+
+  if (x == -1 && y == -1) {
+    std::cout << "#EROARE: We did not find the king in my vector" << std::endl;
+    return false;
   }
 
   // true - check
@@ -427,13 +432,18 @@ bool Game::is_check_m() {
 }
 
 bool Game::is_check_e() {
-  char x, y;
+  char x = -1, y = -1;
   for (auto &v : e_pieces) {
     if (v[2] == 7) {  // my king position
       x = v[0];
       y = v[1];
       break;
     }
+  }
+
+  if (x == -1 && y == -1) {
+    std::cout << "#EROARE: We did not find the king in the enemy's vector" << std::endl;
+    return 0;
   }
 
   // true - check
